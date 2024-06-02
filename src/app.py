ca93d07f7825b9df6fec1963048926f1f3daa339
SergioLP99 <a18490949@itmexicali.edu.mx>
2024-06-02 00:24:43 -0700

    Practica login 2, primer commit

--- src/app.py
+++ src/app.py
@@ -1,6 +1,17 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user 
+from flask_mysqldb import MySQL
+from models.modelUsers import ModelUsers
+from models.entities.users import User
 from config import config
 app = Flask(__name__)
+db = MySQL(app)
+login_manager_app = LoginManager(app)
+
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUsers.get_by_id(db, id)
 
 @app.route('/')
 def root():
@@ -9,13 +20,14 @@ def root():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        _user = request.form["username"]
-        _pass = request.form["password"]
-        print(_user)
-        print(_pass)
-        if _user == "admin" and _pass == "123":
+        user = User(0, request.form['username'], request.form['password'], 0)
+        logged_user = ModelUsers.login(db, user)
+        print("Logged User:", logged_user)  # Debug print
+        if logged_user is not None:
+            login_user(logged_user)
             return redirect(url_for("home"))
         else:
+            flash('Acceso rechazado.', 'danger')
             return render_template("auth/login.html")
     else:
         return render_template("auth/login.html")
